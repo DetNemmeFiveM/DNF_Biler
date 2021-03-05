5328b06c90ccc2de57bba978c74dd762e942bfe1
DetNemmeFiveM <80092528+DetNemmeFiveM@users.noreply.github.com>
2021-03-05 09:48:55 +0100

    Add files via upload

---
+++ LND_ELS/vcf.lua
@@ -0,0 +1,25 @@
+vcf_files = {
+	-- Politi
+	"2012mercedes.xml",
+	"2012MercedesCivil.xml",
+	"2015mercedes.xml",
+	"2020passatFacelift.xml",
+	"2015PassatCivil.xml",
+	"2019volvoV90Hundepatrulje.xml",
+	"2015passatLED.xml",
+	"2016MondeoHund.xml",
+	"SprinterGruppevogn.xml",
+	"SprinterKST.xml",
+	"T6GruppeBlaa.xml",
+	"T6Indsatsleder.xml",
+	"YamahaPOLITI.xml",
+	--Læge
+	"2018mercedesAmbulance.xml",
+	"2016skodaCombi.xml",
+	"2016skodaSuperbCombi.xml",
+	"2018arteon.xml",
+	"2020passatFacelift.xml",
+	"2017volvoV60.xml",
+	"EMSRS6.xml",
+	"EMSXC90.xml"
+}
